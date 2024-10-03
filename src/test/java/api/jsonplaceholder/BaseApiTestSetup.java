c0e274fabf0e5cda9078c6a5ad021828ad0efa50
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2024-10-03 20:23:42 -0300

    feat: Create Base test setup class

---
+++ src/test/java/api/jsonplaceholder/BaseApiTestSetup.java
@@ -0,0 +1,25 @@
+package api.jsonplaceholder;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.qameta.allure.restassured.AllureRestAssured;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+public class BaseApiTestSetup {
+    @BeforeEach
+    public void setup() {
+        // URI base para os testes
+        RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
+
+        // Verifica se os filtros já foram adicionados (Evitar relatório com valores duplicados à cada execução de cada teste)
+        List<?> currentFilters = RestAssured.filters();
+        if (currentFilters.isEmpty()) {
+            RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
+            RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new AllureRestAssured());
+        }
+    }
+}
